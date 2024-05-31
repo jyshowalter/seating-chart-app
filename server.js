fccde857936e0385e2d319fc44cf7a8976a8d8f4
Jason Showalter <jyshowalter@gmail.com>
2024-05-31 08:51:39 -0700

    updated api connection

--- server.js
+++ server.js
@@ -29,6 +29,27 @@ app.post("/buyTicket", (request, response) => {
     .catch((error) => console.error(error));
 });
 
+app.put("/markUncomplete", (request, response) => {
+  db.collection("seats")
+    .updateOne(
+      { thing: request.body.itemFromJS },
+      {
+        $tickets: {
+          isChecked: false,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: false,
+      }
+    )
+    .then((result) => {
+      console.log("Marked Unbought");
+      response.json("Marked Unbought");
+    })
+    .catch((error) => console.error(error));
+});
+
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
